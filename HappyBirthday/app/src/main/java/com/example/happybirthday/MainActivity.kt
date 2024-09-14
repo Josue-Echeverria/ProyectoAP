6695d0a13195a7ee8edd61022bc8398b49307201
Josue <jecheverria@estudiantec.cr>
2024-09-14 12:06:43 -0600

    Refactoring code base

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.happybirthday
 
-import androidx.appcompat.app.AppCompatActivity
+
+//import android.R
+import SecondActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val donacionesText = findViewById<TextView>(R.id.donaciones_text)
+        donacionesText.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
 }
